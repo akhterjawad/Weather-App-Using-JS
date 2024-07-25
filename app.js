01348817be1c9c2447e4dd543231ab1d2e88025f
Muhammad Jawad <akhterjawad566@gmail.com>
2024-07-25 17:42:52 +0500

    complete

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", (event) => {
             card.innerHTML = `
                 <div class='card-custom mx-auto mt-5' style='max-width: 600px;'>
                     <h1 class='h3'>${res.data.location.name}</h1>
-                    <p class='text-muted'>${res.data.location.localtime}, ${res.data.location.country}</p>
+                    <p class='text-muted'>${res.data.location.localtime}</p>
                     <div class='d-flex justify-content-between align-items-center'>
                         <h2 class='display-3'>${res.data.current.temp_c}°C</h2>
                         <img class='weather-icon' src=${res.data.current.condition.icon} alt='Weather Icon' />
